ab1014bed562f54c21883011de705371032db1e9
hothiptv <hothiptvnek@gmail.com>
2026-07-12 22:08:20 +0700

    Add background data for various themes

---
+++ dt.js
@@ -0,0 +1,26 @@
+const backgroundsData = {
+    "Alcs": {
+        "bgAuthor": "MinhCucjo93",
+        "bgName": "AllineCs",
+        "bgUrl": "https://raw.githubusercontent.com/hothiptv/AllineCs/refs/heads/main/ALLINECS.png",
+        "bgVersion": "v1.0"
+    },
+    "abgeo": {
+        "bgAuthor": "Hothiptv",
+        "bgName": "abgeo",
+        "bgUrl": "https://raw.githubusercontent.com/hothiptv/Ancall/main/Untitled%20design.png",
+        "bgVersion": "v1.0"
+    },
+    "bootstrap": {
+        "bgAuthor": "Hothiptv",
+        "bgName": "BootsTrap",
+        "bgUrl": "https://github.com/hothiptv/AllineCs/raw/refs/heads/main/B.png",
+        "bgVersion": "v1.0"
+    },
+    "cybe": {
+        "bgAuthor": "Gameconvl",
+        "bgName": "Cyber Beacon",
+        "bgUrl": "https://github.com/hothiptv/AllineCs/raw/refs/heads/main/Cyber%20Beacon.png",
+        "bgVersion": "V1.0"
+    }
+};
